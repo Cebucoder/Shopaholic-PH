466d48c98902a2a8b2b508684242dbdf35bbb98c
Cebucoder <danielabellana187@gmail.com>
2023-01-17 21:26:47 -0800

    initial

--- js/account.js
+++ js/account.js
@@ -3,3 +3,109 @@ let account = document.getElementById("Account");
 function openAccount() {
 	account.classList.toggle("toggle-account");
 }
+
+
+// checot script function
+
+let credit = document.getElementById("credit1");
+let debit = document.getElementById("debit1");
+let paypal = document.getElementById("paypal1");
+let gcash = document.getElementById("gcash1");
+
+
+let creditform = document.getElementById('credit-form');
+let debitform = document.getElementById('debit-form');
+let paypalform = document.getElementById('paypal-form');
+let gcashform = document.getElementById('gcash-form');
+
+function credit1() {
+
+		// showing the form that I hide 
+	creditform.classList.add('credit-form-click');
+
+	// debitform.classList.remove('debit-form-click');
+	paypalform.classList.remove('paypal-form-click');
+	gcashform.classList.remove('gcash-form-click');
+
+
+	// adding border effect by classlist*(form css)
+	credit.classList.add('credit-click');
+	debit.classList.remove('debit-click');
+	paypal.classList.remove('paypal-click');
+	gcash.classList.remove('gcash-click');
+
+
+}
+function debit1() {
+		// showing the form that I hide 
+	// debitform.classList.add('debit-form-click');
+	creditform.classList.remove('credit-form-click');
+	paypalform.classList.remove('paypal-form-click');
+	gcashform.classList.remove('gcash-form-click');
+
+	// adding border effect by classlist*(form css)
+	credit.classList.remove('credit-click');
+	debit.classList.add('debit-click');
+	paypal.classList.remove('paypal-click');
+	gcash.classList.remove('gcash-click');
+}
+function paypal1() {
+
+		// showing the form that I hide 
+	paypalform.classList.add('paypal-form-click');
+	creditform.classList.remove('credit-form-click');
+	// debitform.classList.remove('debit-form-click');
+	gcashform.classList.remove('gcash-form-click');
+	
+	// adding border effect by classlist*(form css)
+	credit.classList.remove('credit-click');
+	debit.classList.remove('debit-click');
+	paypal.classList.add('paypal-click');
+	gcash.classList.remove('gcash-click');
+}
+function gcash1() {
+
+	// showing the form that I hide
+	creditform.classList.remove('credit-form-click');
+	// debitform.classList.remove('debit-form-click');
+	paypalform.classList.remove('paypal-form-click');
+	gcashform.classList.add('gcash-form-click'); 
+	
+
+	// adding border effect by classlist*(form css)
+	credit.classList.remove('credit-click');
+	debit.classList.remove('debit-click');
+	paypal.classList.remove('paypal-click');
+	gcash.classList.add('gcash-click');
+}
+
+
+// account popup modal
+let modal = document.getElementById('Modal-Popup');
+let close = document.getElementById('Modal-Popup');
+let save = document.getElementById('Modal-Popup');
+
+
+function openModal() {
+	modal.classList.add('open-modal');
+}
+
+function closeModal() {
+	close.classList.remove('open-modal');
+}
+function saveModal() {
+	close.classList.remove('open-modal');
+}
+
+// popup recipt function
+let reciept = document.getElementById('reciept-popup');
+
+
+function openRec() {
+	reciept.classList.add('toggle-reciept');
+}
+
+let closecheck = document.getElementById('reciept-popup');
+function closeCheck() {
+	closecheck.classList.remove('toggle-reciept');
+}
---
+++ js/web.js
@@ -0,0 +1,128 @@
+// script for Popup cart
+
+
+	
+let cartPop = document.getElementById("cart-pop-cont");
+let closeCart = document.getElementById("cart-pop-cont");
+
+function cartPopup() {
+	cartPop.classList.add("toggle-cart");
+	openSlide.classList.remove("toggle-home");
+	dashboard.classList.remove("toggle-dashboard");
+}
+function closeCartpopup(){
+	closeCart.classList.remove("toggle-cart");
+}
+
+
+
+
+// toggele the home slide
+
+	
+let openSlide = document.getElementById("open-home");
+let dashboard = document.getElementById("openDashboard");
+
+
+function openHome() {
+	openSlide.classList.toggle("toggle-home");
+	dashboard.classList.remove("toggle-dashboard");
+	cartPop.classList.remove("toggle-cart");
+
+}
+
+function openDashboard() {
+	dashboard.classList.toggle("toggle-dashboard");
+	openSlide.classList.remove("toggle-home");
+	cartPop.classList.remove("toggle-cart");
+}
+
+
+// add to cart remove funtion
+
+let remove = document.getElementById('remove');
+
+function removeFun() {
+	remove.remove('remove');
+}
+
+let remove1 = document.getElementById('remove1');
+
+function removeFun1() {
+	remove1.remove('remove1');
+}
+
+let remove2 = document.getElementById('remove2');
+
+function removeFun2() {
+	remove2.remove('remove2');
+}
+
+let remove3 = document.getElementById('remove3');
+
+function removeFun3() {
+	remove3.remove('remove3');
+}  
+
+
+// toogle navlink mobile
+let menu = document.getElementById("navlink-mobile-open");
+
+	function menuOpen(){
+		menu.classList.toggle("navlink-mobile-open");
+	}
+
+
+// js code for hovering prodcut view
+
+var ProductImg = document.getElementById("ProductImg");
+var SmallImg = document.getElementsByClassName("smallImg");
+
+SmallImg[0].onmouseover = function()
+{
+	ProductImg.src = SmallImg[0].src;
+}
+SmallImg[1].onmouseover = function()
+{
+	ProductImg.src = SmallImg[1].src;
+}
+SmallImg[2].onmouseover = function()
+{
+	ProductImg.src = SmallImg[2].src;
+}
+SmallImg[3].onmouseover = function()
+{
+	ProductImg.src = SmallImg[3].src;
+}
+
+
+
+// add to cart script
+	
+var noti = document.getElementById('cart');
+// var select = document.querySelector('select');
+var select = document.getElementById('button');
+
+{
+	select.addEventListener('click', (e)=>{
+		var add = Number(noti.getAttribute('cart-count')|| 0);
+		noti.setAttribute('cart-count', add + 1);
+		noti.classList.add('zero');
+	})
+
+} 
+
+
+
+
+// script to toggle heart background 
+
+
+	
+let wish = document.getElementById('hearted');
+
+function wishOpen() {
+	wish.classList.toggle('wish-open');
+}
+
+
